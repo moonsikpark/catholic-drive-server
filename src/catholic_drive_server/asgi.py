2e56c4c738839281773784d7a61d6d9a9395a269
Moonsik Park <moonsik.park@estsoft.com>
2024-08-26 12:55:11 +0900

    feat: initial project setup

---
+++ src/catholic_drive_server/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for catholic_drive_server project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'catholic_drive_server.settings')
+
+application = get_asgi_application()
